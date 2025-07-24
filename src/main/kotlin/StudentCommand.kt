f0495503c51ca1f1e6eb805e09ad6c8291383603
samaahmed19 <sama23012612@gmail.com>
2025-07-24 22:36:01 +0300

    Add fun remove,update

--- src/main/kotlin/StudentCommand.kt
+++ src/main/kotlin/StudentCommand.kt
@@ -7,4 +7,8 @@ sealed class StudentCommand {
     object GetStudents : StudentCommand()
     data class GetStudentsByGrade(val grade: String) : StudentCommand()
     data class GetStudentsByAge(val age: Int) : StudentCommand()
+    data class RemoveStudentByName(val name: String) : StudentCommand()
+    data class RemoveStudentById(val id: Int) : StudentCommand()
+    data class UpdateStudentByName(val name: String, val updatedStudent: Student) : StudentCommand()
+    data class UpdateStudentById(val id: Int, val updatedStudent: Student) : StudentCommand()
 }
