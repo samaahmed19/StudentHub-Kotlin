cae642b4956e54594321a42097b8b61a590e91bd
Ahmed Salah Aldin Ahmed <92693085+ahmed-sala@users.noreply.github.com>
2025-07-24 18:07:41 +0300

    Add Manager class to handle student operations

---
+++ src/main/kotlin/Manager.kt
@@ -0,0 +1,21 @@
+package org.example
+
+import kotlin.collections.mutableListOf
+
+class Manager {
+    private val students = mutableListOf<Student>()
+    
+    fun addStudent(student: Student) {
+        students.add(student)
+    }
+    fun getStudents(): List<Student> {
+        return students
+    }
+
+    fun getStudentsByGrade(grade: String): List<Student> {
+        return students.filter { it.grade == grade }
+    }
+    fun getStudentsByAge(age: Int): List<Student> {
+        return students.filter { it.age == age }
+    }
+}
